39740f0157af8f3ae7dc66c4049e2c6d07690e0e
AritriBasu <59471237+AritriBasu@users.noreply.github.com>
2020-11-23 00:47:03 +0530

    added OAuth2

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+//dotenv
+require('dotenv').config();
+
 const express=require('express');
 const app=express();
 const path=require('path');
 const bodyParser = require('body-parser');
 const exphbs=require('express-handlebars');
 const nodemailer = require('nodemailer');
-//dotenv
-require('dotenv').config();
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+
+
 //static folder
 app.use('/public',express.static(path.join(__dirname,'public')));
 
@@ -26,6 +31,19 @@ app.get('/',function(req,res){
 });
  
 
+//setting up oauth client
+const oauth2Client = new OAuth2(
+  "654178036658-3ou45h2i2fdh65481e3j15ehbl3mgbs6.apps.googleusercontent.com", // ClientID
+  "gQD7LYymi-pev3xqXV46pt0U", // Client Secret
+  "https://developers.google.com/oauthplayground" // Redirect URL
+);
+
+//refresh and acess token
+oauth2Client.setCredentials({
+  refresh_token: "1//04lHiMVdIQWlkCgYIARAAGAQSNwF-L9IrPAhvNIlBkeelS721vIvBZI5RD8TV0ih11JxuMS6wZGzTP5kKGbmlrxN_BgWp-1uPilc"
+});
+const accessToken = oauth2Client.getAccessToken()
+
 //to get form data
 app.post('/submit-data', function (req, res) {
   const output=`
@@ -42,16 +60,22 @@ app.post('/submit-data', function (req, res) {
     <h3>Requirement: <p>${req.body.message}</p></h3>
   `
   //nodemailer syntax
-  var transporter = nodemailer.createTransport({
-    service: 'gmail',
+
+  const smtpTransport = nodemailer.createTransport({
+    service: "gmail",
     auth: {
-      user: 'roleplast1101@gmail.com',
-      pass: 'Roleplast9876#'
+         type: "OAuth2",
+         user: "roleplast1101@gmail.com", 
+         clientId: "654178036658-3ou45h2i2fdh65481e3j15ehbl3mgbs6.apps.googleusercontent.com",
+         clientSecret: "gQD7LYymi-pev3xqXV46pt0U",
+         refreshToken: "1//04lHiMVdIQWlkCgYIARAAGAQSNwF-L9IrPAhvNIlBkeelS721vIvBZI5RD8TV0ih11JxuMS6wZGzTP5kKGbmlrxN_BgWp-1uPilc",
+         accessToken: accessToken
     },
     tls:{
       rejectUnauthorized:false
     }
-  });
+});
+
   
   var mailOptions = {
     from: 'roleplast1101@gmail.com',
@@ -60,7 +84,7 @@ app.post('/submit-data', function (req, res) {
     html: output
   };
   
-  transporter.sendMail(mailOptions, function(error, info){
+  smtpTransport.sendMail(mailOptions, function(error, info){
     if (error) {
       console.log(error);
       const errmsg="Your message could not be delivered. Please retry or contact us using the details below Hoping to hear from you soon!"
@@ -72,9 +96,10 @@ app.post('/submit-data', function (req, res) {
   const sentmsg="Your request has been submitted. We will get back to you shortly!";
   //after submission
   res.render('index',{msg:sentmsg});
-
+  smtpTransport.close();
 });
 
+//adding port for heroku
 var port = process.env.PORT || 3000;
 app.listen(port,function(){
   console.log('Server is running on http://localhost:' + port);
